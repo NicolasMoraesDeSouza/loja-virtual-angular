528d2f7ed119ed89e929ca605bfb3eb601b40e04
Nicolas Moraes de Souza <67681@PROWAY.TREINA>
2021-02-17 21:38:40 -0300

    Criando componente de rodape

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  template: './footer.component.html',
+  styles: [
+  ]
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
