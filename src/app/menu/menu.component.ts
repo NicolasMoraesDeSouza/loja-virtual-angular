d33abc8e5db56ff9407b24c5580f053ddecff999
Nicolas Moraes de Souza <67681@PROWAY.TREINA>
2021-02-17 22:06:29 -0300

    reestrutura do app menu

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  template: './menu.component.html',
+  styles: [
+  ]
+})
+export class MenuComponent {
+
+  constructor() { }
+
+  
+  }
+
+
