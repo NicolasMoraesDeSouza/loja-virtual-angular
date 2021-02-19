13efbb4e80b45495da81c17b289e3432e75d226b
Nicolas Moraes de Souza <67681@PROWAY.TREINA>
2021-02-18 22:08:09 -0300

    adicionando a tela de sobre e contato

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { ContatoComponent } from './institucional/contato/contato.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { HomeComponent } from './navegacao/home/home.component';
 
@@ -6,5 +7,6 @@ export const rooteRouterConfig: Routes = [
     
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},    
-    {path: 'sobre', component: SobreComponent}
+    {path: 'sobre', component: SobreComponent},
+    {path: 'contato', component: ContatoComponent}
 ];
