a231f0bb3ec5ab573feef40231886ba265541d65
Nicolas Moraes de Souza <67681@PROWAY.TREINA>
2021-02-18 21:55:21 -0300

    adicionando rotas na barra superior

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './navegacao/home/home.component'
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { HomeComponent } from './navegacao/home/home.component';
 
 export const rooteRouterConfig: Routes = [
     
-    {path: '', redirectTo: './home', pathMatch: 'full'},
-    {path: 'home', component: HomeComponent}
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},    
+    {path: 'sobre', component: SobreComponent}
 ];
